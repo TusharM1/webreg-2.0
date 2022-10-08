8da434d9f0c15562c8bbda349e29f39511f4aeb5
Tushar Mahajan <tman08810@gmail.com>
2022-10-08 17:07:45 -0400

    Add navigation pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./pages/Header";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Footer from "./pages/Footer";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Header/>
+            <Routes>
+                <Route path="/">
+                    <Route path="login" element={<Login/>} />
+                    <Route path="dashboard" element={<Dashboard/>} />
+                </Route>
+            </Routes>
+            <Footer/>
+        </BrowserRouter>
+    );
 }
 
 export default App;
