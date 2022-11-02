204ab8df4aad8bb8b66b723378a9999ec7f64135
Fenguinn <salem.abdulla15@gmail.com>
2022-11-02 15:38:42 -0400

    add meeting time loop

--- backend/apiParse.js
+++ backend/apiParse.js
@@ -16,13 +16,20 @@ request(url, options, (error, res, body) => {
         //console.log(body.title)
         //console.log(typeof(body))
        // console.log(body[30]["title"])
-       for (i = 0; i<3; i++){
+       console.log(body[0]);
+       for (i = 0; i<0; i++){
             console.log(body[i]["title"])
             console.log(body[i]["openSections"])
            // console.log(body[i]["sections"])
             for (j=0; j<body[i]["sections"].length; j++){
-                //console.log(body[i]["sections"][j]["meetingTimes"])
-                console.log(body[i]["sections"][j]["index"])
+                //console.log(body[i]["sections"][j]["meetingTimes"][0]['startTime'])
+                //console.log(body[i]["sections"][j]["meetingTimes"][0]['endTime'])
+                for (k = 0; k < body[i]["sections"][j]["meetingTimes"].length; k++){
+                    console.log(body[i]["sections"][j]["meetingTimes"][k]['pmCode'])
+                    console.log(body[i]["sections"][j]["meetingTimes"][k]['startTime'])
+                    console.log(body[i]["sections"][j]["meetingTimes"][k]['endTime'])
+                }
+                //console.log(body[i]["sections"][j]["index"])
             }
             // for (j=0; j<body[i].length; j++){
             //     if (body[i][j] === )
