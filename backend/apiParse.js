f4c345cb7377c821d30c8269b123e348c6b0239c
Fenguinn <salem.abdulla15@gmail.com>
2022-11-09 21:46:07 -0500

    Finishing Section data Complete, Section Block v0.5

--- backend/apiParse.js
+++ backend/apiParse.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 const { getSystemErrorMap } = require('util');
 
 let content = '';
-
+let sectionPrint ='';
 
 request(url, options, (error, res, body) => {
     if (error) {
@@ -17,8 +17,11 @@ request(url, options, (error, res, body) => {
 
     if (!error && res.statusCode == 200) {
         
-        console.log(body[0]);
-
+        //console.log(body[0]);
+        //console.log(body[0]['sections'][0]);
+        //console.log(body[0]["sections"][0]['instructors'][0]['name']);
+        //console.log(body[10]);
+        //console.log(body[10]['sections'][0]['meetingTimes']);
        for (i = 0; i<body.length; i++){
             let contentCourseString = `'${body[i]['offeringUnitCode']}:${body[i]['subject']}:${body[i]['courseNumber']}'`
             let contentSchoolNumber = `'${body[i]['offeringUnitCode']}'`
@@ -57,20 +60,75 @@ request(url, options, (error, res, body) => {
               });
             
             for (j=0; j<body[i]["sections"].length; j++){ //Sections
- 
-                for (k = 0; k < body[i]["sections"][j]["meetingTimes"].length; k++){ //section meeting times
-                    //console.log(body[i]["sections"][j]["meetingTimes"][k]['pmCode'])
-                    //console.log(body[i]["sections"][j]["meetingTimes"][k]['startTime'])
-                    //console.log(body[i]["sections"][j]["meetingTimes"][k]['endTime'])
+                let sectionIndex = `'${body[i]["sections"][j]['index']}'`; //string
+                let courseString = `${contentCourseString}`; //string
+                let sectionNumber = `'${body[i]["sections"][j]['number']}'`;//string
+                let sectionType = ``; //string gotta do this in the loop
+                let professor;
+                try {
+                    professor = `'${body[i]["sections"][j]['instructors'][0]['name']}'`; //string
                 }
-                //console.log(body[i]["sections"][j]["index"])
+                catch (e){
+                    professor = `'null'`;
+                }
+                
+                let capacity = 5; //Setting default to 5 for now
+                let comments = '';
+                try {
+                    comments = `'${body[i]["sections"][j]['comments'][0]['description']}'`; //string    
+                } 
+                catch (e){
+                    comments = `'null'`;
+                }
+                
+
+                
+                const Asynchronous = body[i]["sections"][j]["meetingTimes"].length
+                const In_Person = 0;
+                let sectionMeetingTypeCounter = 0;
+
+                for (k = 0; k < body[i]["sections"][j]["meetingTimes"].length; k++){ //section meeting times aka section BLOCK
+                    let sectionIndex = `'${body[i]["sections"][j]['index']}'`; //String
+                    let blockDay = `'${body[i]["sections"][j]['meetingTimes']['meetingDay']}'`; //String
+                    let blockStart =`'${body[i]["sections"][j]['meetingTimes']['startTime']}'`; //Time
+                    let blockEnd = `'${body[i]["sections"][j]['meetingTimes']['endTime']}'`; //Time
+                    let location = `'${body[i]["sections"][j]['meetingTimes']['campusLocation']}'`;//String
+                    let meetingType = ``;
+                    if (location == 'O'){
+                        meetingType = `'Online'`; //String
+                        sectionMeetingTypeCounter++;
+                    }
+                    else {
+                        meetingType = `'In-Person'`; //String
+                    }
+                    let SectionBlockPrint = `INSERT INTO sectionBlock VALUES ( ${sectionIndex}, ${blockDay}, ${blockStart}, ${blockEnd}, ${location}, ${meetingType});\n`;
+                    fs.appendFile('./csSectionsBlocks.txt', SectionBlockPrint, err => {
+                        if (err) {
+                          console.error(err);
+                        }
+                      });
+                    
+                }
+
+                if (sectionMeetingTypeCounter >= Asynchronous){
+                    sectionType = `'Asynchronous'`;
+                }
+                else if (sectionMeetingTypeCounter <= 0){
+                    sectionType = `'In-Person'`;
+                }
+                else {
+                    sectionType = `'Hybrid'`;
+                }
+
+                sectionPrint = `INSERT INTO section VALUES ( ${sectionIndex}, ${courseString}, ${sectionNumber}, ${sectionType}, ${professor}, ${capacity}, ${comments});\n`;
+                fs.appendFile('./csSections.txt', sectionPrint, err => {
+                    if (err) {
+                      console.error(err);
+                    }
+                  });
             }
-            // for (j=0; j<body[i].length; j++){
-            //     if (body[i][j] === )
-            //     console.log(body[i][j][])
-            // }
+            
        }
-      // console.log(body.length)
     };
     
 });
