6f770ddc38c6c039afa67f8223c5c7cfe088e23c
Gent B <gentblaku@gmail.com>
2022-11-14 01:53:09 -0500

    Trying to create a course based on admin input

--- frontend/src/components/AdminUI.js
+++ frontend/src/components/AdminUI.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import { Accordion, Container } from "react-bootstrap";
+import {Course} from '../backend/models/Course.js'
 
 
 
 
 
 
-const  createClass= async (courseString, name, credits) => {
-    const data = await axios.post("http://localhost:3001/auth", {
-        type: "login",
-        data: {
-            netID: netID,
-            password: password
-        }
-    }).then(response => response.data);
-    const saved = saveData(data);
-    if (saved) {
-        navigate("/dashboard");
-    }
-    console.log(data["message"]);
-};
+// const  createClass= async (courseString, name, credits) => {
+//     const data = await axios.post("http://localhost:3001/auth", {
+//         type: "login",
+//         data: {
+//             netID: netID,
+//             password: password
+//         }
+//     }).then(response => response.data);
+//     const saved = saveData(data);
+//     if (saved) {
+//         navigate("/dashboard");
+//     }
+//     console.log(data["message"]);
+// };
 
 
 
@@ -30,28 +31,20 @@ const  createClass= async (courseString, name, credits) => {
 export function AdminUI() {
     return (
         <Container fluid>
-            onSubmit= createClass {async (values, actions) => {
-                        await (values["courseString"], values["name"], values["credits"]);
-                        actions.resetForm();
-                    }}
-            {/* <div className={"h-25 bg-light-coral"}>
-                <span>Search Courses</span>
-            </div>
-            <span>View Courses</span>
-            <div className={"d-flex justify-content-between w-100"}>
-                <span>Course String</span>
-                <span>Course Name</span>
-            </div> */}
-            <div>
-                <input name='courseString' placeholder='00:000:000'></input>\
+            <form>
+                <input name='courseString' placeholder='00:000:000'></input>
                 <input name='name' placeholder='Name'></input>
                 {/* <input id='description' placeholder='Description'></input> */}
                 <input name='credits' placeholder='Number of credits'></input>
                 {/* <input id='prerequisites' placeholder='Prerequisites'></input> */}
-                <button type='submit' id='createClass'>
+                <button type='submit' id='createClass' 
+                    onSubmit={async (values, actions) => {
+                        await Course.create(values["courseString"], values["courseString"].subString(0,2), values["courseString"].subString(3,6), values["courseString"].subString(7,10), values["name"], "null", values["credits"],"null");
+                        actions.resetForm();
+                    }}>
                     CreateClass
                 </button>
-            </div>
+            </form>
         </Container>
     );
 }
